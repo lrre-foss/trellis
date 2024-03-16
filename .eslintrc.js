3b9053127760823690a0475fb4c8cc4ea20c40b4
rjindael <me@rjindael.xyz>
2024-03-15 19:45:17 -0700

    chore: more project scaffolding
    mostly lifted from https://github.com/PLhery/node-twitter-api-v2

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+
+    "parser": "@typescript-eslint/parser",
+    "plugins": ["@typescript-eslint"],
+    "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+
+    "rules": {
+        "semi": ["error", "never"],
+        "quotes": ["error", "double"]
+    },
+};
